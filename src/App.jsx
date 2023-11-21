ca72d26e38d9575e2b77c6afd5a7e75dffc0f968
Javad <faraji_javad81@yahoo.com>
2023-11-21 23:39:15 +0330

    Navbar modification, language changed, logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Navbar from "./components/navbar/navbar.jsx";
 function App() {
-    const elem = [];
-    for (let i = 0; i < 100; i++) {
-        elem.push(<br/>);
-    }
+  const elem = [];
+  for (let i = 0; i < 100; i++) {
+    elem.push(<br />);
+  }
 
   return (
     <>
-        <Navbar isLogin={false} />
-        {elem}
+      <Navbar isLogin={false} />
+      {elem}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/login&signup/login.jsx
+++ src/components/navbar/login&signup/login.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function Login (){
-    return(
-        <>
-            <a
-                href="#"
-                className="nav-link border border-white px-4 py-2 rounded-3xl nav-link-sm"
-            >
-                ورود / ثبت نام
-            </a>
-        </>
-    )
+export default function Login() {
+  return (
+    <>
+      <a
+        href="#"
+        className="flex items-center p-5 bg-amber-400 shadow-lg place-items-center text-black px-10 py-3 rounded-full hover:bg-amber-300 hover:shadow-inner"
+      >
+        Login / Signup
+      </a>
+    </>
+  );
 }
