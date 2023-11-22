eb02c7b027b87123ee77e613ffcfd0db4d8d6c6c
armin40behrouzi81 <armin40behrouzi81@gmail.com>
2023-11-22 10:36:43 +0330

    ManagerPanel: Add ShowStars component

--- src/components/ManagerPanel/ManagerPanel.jsx
+++ src/components/ManagerPanel/ManagerPanel.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import  './ManagerPanel.css'
+import ShowStars from './ShowStars';
 
 const ManagerPanel = () => {
+
     return (
-        <div className="text-3xl text-center font-bold underline">
-            <h1>Manager Panel</h1>
-        </div>
+        <>
+            <div className="text-3xl text-center font-bold underline">
+                <h1>Manager Panel</h1>
+            </div>
+
+            <div class="flex items-center">
+                <ShowStars StarsNumbers='3'/>
+            </div>
+            
+        </>
     );
 }
  
