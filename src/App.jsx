0bc6b919f13554164db105b7de831a455e8df65e
Arrash03 <atv1382@gmail.com>
2023-11-26 22:17:54 +0330

    manager panel added to pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Admin from "./layouts/admin/index.jsx";
 import Footer from "./components/footer/footer.jsx";
+import ManagePanel from "./pages/panel/manager/managePanel.jsx";
 function App() {
     return (
-        <div className="flex flex-col ">
-            <div>
-                <Routes>
-                    <Route path="admin/*" element={<Admin/>}/>
-                    <Route path="/" element={<Navigate to="/admin" replace/>}/>
-                </Routes>
-            </div>
-            <Footer />
-        </div>)
+        <>
+            <ManagePanel />
+        </>
+    )
 }
 export default App;
