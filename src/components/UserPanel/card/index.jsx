8f8242898d7c41b38ad0d3b57767ff39da1107ec
Javad <faraji_javad81@yahoo.com>
2023-12-10 18:13:17 +0330

    card color modified

--- src/components/UserPanel/card/index.jsx
+++ src/components/UserPanel/card/index.jsx
@@ -2,7 +2,7 @@ function Card(props) {
   const { variant, extra, children, ...rest } = props;
   return (
     <div
-      className={`!z-5 relative flex flex-col rounded-[20px] bg-yellow-300 bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:shadow-none ${extra}`}
+      className={`!z-5 shadow-3xl shadow-shadow-500 dark:!bg-navy-800 relative flex flex-col rounded-[20px] bg-amber-300 bg-clip-border dark:text-white dark:shadow-none ${extra}`}
       {...rest}
     >
       {children}
