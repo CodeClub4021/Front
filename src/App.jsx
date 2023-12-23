1230caea761e70d4fbf75fc5370dae6241c77c48
Ali Bagheri <69904518+aibgr@users.noreply.github.com>
2023-12-23 22:49:31 +0330

    gym win created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home.jsx";
 import ManagePanel from "./pages/panel/manager/managePanel";
 import CoachPanel from "./pages/panel/coach/coachPanel.jsx";
 import UserPanel from "./pages/panel/user/userpanel";
+import Gym from "./pages/gym_win/gym_win"
 import React from "react";
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
     <>
       {/* <Land/> */}
       {/* <Home /> */}
-      <ManagePanel />
+      {/* <ManagePanel /> */}
             {/* <CoachPanel/> */}
       {/* <UserPanel /> */}
+    <Gym />
       {/* <Login /> */}
     </>
   );
