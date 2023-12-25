615eee6f85db87711068de73b6a6e256abdeab25
armin40behrouzi81 <armin40behrouzi81@gmail.com>
2023-12-25 21:31:48 +0330

    About Us v1.0

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,37 +1,52 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import routes from "../../components/ManagerPanel/sidebar/routes.jsx";
 import NavbarJavad from "../../components/navbar/navbar.jsx";
 import Footer from "../../components/footer/footer.jsx";
+import Banner from "./components/AboutBanner.jsx";
 
-export default function AboutUs(props) {
+export default function AboutUs() {
 
   return (
-    // <div className="flex h-full w-full">
-    //   {/* Navbar & Main Content */}
-    //   <div className="h-full w-full bg-gray-900 dark:!bg-navy-900">
-    //     {/* Main Content */}
-    //     <main
-    //       className={`mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]`}
-    //     >
-    //       {/* Routes */}
-    //       <div className="h-full">
-    //         <NavbarJavad/>
-    //         <div className="pt-5s mx-auto mb-auto h-full min-h-[84vh] p-2 md:pr-2">
-    //           <Routes>
-    //             {getRoutes(routes)}
+    <div className="flex h-full w-full">
+      {/* Navbar & Main Content */}
+      <NavbarJavad/>
+      <div className="h-full w-full bg-gray-900 dark:!bg-navy-900">
+        {/* Main Content */}
+        <main className={`mx-[12px] h-full flex-none transition-all mt-20 mb-10 md:pr-2 xl:ml-[30px]`}>
+            <div className="text-6xl font-bold text-amber-300">About Us </div>
+            <p className="ml-5 mt-2 px-3 text-2xl text-amber-300">
 
-    //             <Route
-    //               path="/"
-    //               element={<Navigate to="/AboutUs/" replace />}
-    //             />
-    //           </Routes>
-    //         </div>
-    //       </div>
-    //     </main>
-    //     {/*<Footer />*/}
-    //   </div>
-    // </div>
-    <h1>salam</h1>
+                Community of code club is a group of students in IUST university. This website is a project of system analysis and design course.
+
+            </p>
+            <div className="text-4xl mt-5 font-bold text-amber-300">Our Team :</div>
+            <div className="flex w-full flex-col gap-5">
+                <div className="w-ful mt-5 flex h-fit flex-col gap-5 lg:grid lg:grid-cols-12">
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                    <div className="col-span-3 m-4 lg:!mb-0">
+                    <Banner />
+                    </div>
+                </div>
+            </div>
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 }
