5e376325c9a35f3a94b9289f7ea2a23039cbd822
armin40behrouzi81 <armin40behrouzi81@gmail.com>
2023-12-25 20:14:43 +0330

    AboutUs started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/login/login";
 import Land from "./pages/landing/land.jsx";
 import Home from "./pages/Home/Home.jsx";
 import ManagePanel from "./pages/panel/manager/managePanel";
+import AboutUs from "./pages/AboutUs/index.jsx";
 import CoachPanel from "./pages/panel/coach/coachPanel.jsx";
 import UserPanel from "./pages/panel/user/userpanel";
 import Gym from "./pages/gym_win/gym_win";
@@ -15,11 +16,12 @@ function App() {
       {/* <Land/> */}
       {/* <Home /> */}
       {/* <ManagePanel /> */}
-            {/* <CoachPanel/> */}
+      {/* <CoachPanel/> */}
       {/* <UserPanel /> */}
       {/* <Gym /> */}
       {/* <Login /> */}
-      <CoachRegister/>
+      {/* <CoachRegister/> */}
+      <AboutUs/>
 
     </>
   );
---
+++ src/pages/AboutUs/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import routes from "../../components/ManagerPanel/sidebar/routes.jsx";
+import NavbarJavad from "../../components/navbar/navbar.jsx";
+import Footer from "../../components/footer/footer.jsx";
+
+export default function AboutUs(props) {
+
+  return (
+    // <div className="flex h-full w-full">
+    //   {/* Navbar & Main Content */}
+    //   <div className="h-full w-full bg-gray-900 dark:!bg-navy-900">
+    //     {/* Main Content */}
+    //     <main
+    //       className={`mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]`}
+    //     >
+    //       {/* Routes */}
+    //       <div className="h-full">
+    //         <NavbarJavad/>
+    //         <div className="pt-5s mx-auto mb-auto h-full min-h-[84vh] p-2 md:pr-2">
+    //           <Routes>
+    //             {getRoutes(routes)}
+
+    //             <Route
+    //               path="/"
+    //               element={<Navigate to="/AboutUs/" replace />}
+    //             />
+    //           </Routes>
+    //         </div>
+    //       </div>
+    //     </main>
+    //     {/*<Footer />*/}
+    //   </div>
+    // </div>
+    <h1>salam</h1>
+  );
+}
