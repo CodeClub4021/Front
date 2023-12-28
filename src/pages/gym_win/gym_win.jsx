43555eebf6bca8848ed7aef0d70456f5595b6032
Javad <faraji_javad81@yahoo.com>
2023-12-28 20:10:56 +0330

    gradient fixed

--- src/pages/gym_win/gym_win.jsx
+++ src/pages/gym_win/gym_win.jsx
@@ -1,26 +1,26 @@
-import './gym_win.css';
-import Footer from '../../components/footer/footer';
-import Hero from '../../components/gym_win/Hero/Hero';
-import Join from '../../components/gym_win/Join/Join';
-import Plans from '../../components/gym_win/Plans/Plans';
-import Programs from '../../components/gym_win/Programs/Programs';
-import Reasons from '../../components/gym_win/Reasons/Reasons';
-import Testimonials from '../../components/gym_win/Testimonials/Testimonials';
+import "./gym_win.css";
+import Footer from "../../components/footer/footer";
+import Hero from "../../components/gym_win/Hero/Hero";
+import Join from "../../components/gym_win/Join/Join";
+import Plans from "../../components/gym_win/Plans/Plans";
+import Programs from "../../components/gym_win/Programs/Programs";
+import Reasons from "../../components/gym_win/Reasons/Reasons";
+import Testimonials from "../../components/gym_win/Testimonials/Testimonials";
 import Navbar from "../../components/navbar/navbar.jsx";
 
 function Gym() {
   return (
     <>
-    <Navbar />
-    <div className="Gym mt-20">     
-      <Hero/>
-      <Programs/>
-      <Reasons/>
-      <Plans/>
-      <Testimonials/>
-      <Join/>
-      <Footer />
-    </div>
+      <Navbar />
+      <div className="Gym mt-20">
+        <Hero />
+        <Programs />
+        <Reasons />
+        <Plans />
+        <Testimonials />
+        <Join />
+        <Footer />
+      </div>
     </>
   );
 }
