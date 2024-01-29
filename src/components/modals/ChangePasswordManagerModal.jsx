25e11b9dd141da764cddf7e053b9c69dbcae555c
armin40behrouzi81 <armin40behrouzi81@gmail.com>
2024-01-29 11:35:14 +0330

    changePassword Connection with error

--- src/components/modals/ChangePasswordManagerModal.jsx
+++ src/components/modals/ChangePasswordManagerModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IoEye , IoEyeOff } from "react-icons/io5";
 import ErrorBoxModal from './ErrorBoxModal';
+import axios from 'axios';
 
 
 const EyeIcon = ({ onClick, isShown }) => {
@@ -21,6 +22,7 @@ const ChangePassword = ({ onClose }) => {
     confirm: false,
   });
   const [formData, setFormData] = useState({
+    userName:'',
     currentPassword: '',
     newPassword: '',
     confirmPassword: '',
@@ -37,25 +39,26 @@ const ChangePassword = ({ onClose }) => {
     return password.length >= 8 && /\d/.test(password);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    if(formData.currentPassword!=="passback"){ //check with back
-      setErrorMessage('The current password is not correct.'); 
-      setShowErrorBoxModal(true);
-    }else if (formData.newPassword !== formData.confirmPassword) {
+     if (formData.newPassword !== formData.confirmPassword) {
       setErrorMessage('The new passwords do not match.'); 
       setShowErrorBoxModal(true);
     } else if (!hasStandardPassword(formData.newPassword)) {
       setErrorMessage('The new password must be at least 8 characters long and include a number.'); 
       setShowErrorBoxModal(true);
     } else {
-        console.log("khaaaaaaaaaaaaaaaaaaaaaaaaat from Manager pass");
         const UpdatedPass = {
-          currentPassword: formData.currentPassword,
-          newPassword: formData.newPassword,
-          confirmPassword: formData.confirmPassword,
+          old_password: formData.currentPassword,
+          new_password: formData.newPassword,
+          username: formData.userName,
         };
-        console.log(UpdatedPass);
+        try{
+          const res = await axios.put("https://codeclub-api.liara.run/changepassword", UpdatedPass);
+          console.log(res);
+        } catch(err){
+         // console.error(err);
+        }
         onClose();
       // Proceed with password change logic...
     }
@@ -100,6 +103,19 @@ const ChangePassword = ({ onClose }) => {
               
                 {/* Form */}
                 <form onSubmit={handleSubmit} className="bg-gray-900 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  {/* Current Password */}
+                  <div className="mb-4 relative">
+                    <label htmlFor="userName" className="block text-lg font-medium text-amber-400">Username</label>
+                    <input
+                      type="text"
+                      name="userName"
+                      id="userName"
+                      value={formData.userName}
+                      onChange={handleInputChange}
+                      className="mt-1 p-2 w-full border rounded-md shadow-sm"
+                      required
+                    />
+                  </div>
                   {/* Current Password */}
                   <div className="mb-4 relative">
                     <label htmlFor="currentPassword" className="block text-lg font-medium text-amber-400">Current Password</label>
