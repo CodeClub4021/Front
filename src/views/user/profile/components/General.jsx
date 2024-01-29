6db5173c51298a731e8955ddb938b53d6b9fc3a9
armin40behrouzi81 <armin40behrouzi81@gmail.com>
2024-01-30 01:35:16 +0330

    User profile fully connected.

--- src/views/user/profile/components/General.jsx
+++ src/views/user/profile/components/General.jsx
@@ -1,89 +1,120 @@
 import Card from "../../../../components/UserPanel/card/";
 import EditProfileCard from "../../../../components/UserPanel/card/EditProfileCard";
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+class General extends Component {
+  state = { 
+    customer: []
+   } 
+   async componentDidMount(){
+    try{
+      const response  = await axios.get('https://gymlist.liara.run/user/',
+      {
+        headers: {
+          Authorization: 'Token ' + localStorage.getItem('token')
+        }
+      });
+      console.log(response);
+      this.setState({customer: response.data.customer})
+    }
+    catch(err){
+      console.log('err')
+      console.error(err);
+    }
 
-const General = () => {
-  return (
-    <Card extra={"w-full h-full p-3"}>
-      {/* Edit Profile */}
-      <div className="ml-auto">
-        <EditProfileCard />
-      </div>
-      {/* Header */}
-      <div className="mb-5 mt-2 w-full">
-        <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
-          General Information
-        </h4>
-      </div>
-      {/* Cards */}
-      <div className="grid grid-cols-3 gap-4 px-2">
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Sex</p>
-          <p className="text-lg font-medium text-white dark:text-white">Male</p>
+  } 
+  render() { 
+    return (
+      <Card extra={"w-full h-full p-3"}>
+        {/* Edit Profile */}
+        <div className="ml-auto">
+          <EditProfileCard />
         </div>
-
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Birthday</p>
-          <p className="text-lg font-medium text-white dark:text-white">
-            20 July 1966
+        {/* Header */}
+        <div className="mb-5 mt-2 w-full">
+          <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
+            General Information
+          </h4>
+        </div>
+        {/* Cards */}
+        <div className="grid grid-cols-3 gap-4 px-2">
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Sex</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* Male */}
+              {this.state.customer.sex}
+            </p>
+          </div>
+  
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Birthday</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* 20 July 1966 */}
+              {this.state.customer.birthday}
+            </p>
+          </div>
+  
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col items-start justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Education</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* Stanford University */}
+              {this.state.customer.education}
+            </p>
+          </div>
+  
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Languages</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* English - Spanish - Italian */}
+              {this.state.customer.language}
+            </p>
+          </div>
+  
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col items-start justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Location</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* USA - LA */}
+              {this.state.customer.location}
+            </p>
+          </div>
+  
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
+            <p className="text-base text-gray-600">Phone number</p>
+            <p className="text-lg font-medium text-white dark:text-white">
+              {/* 09123456789 */}
+              {this.state.customer.phone_number}
+            </p>
+          </div>
+        </div>
+        {/* 
+        <div className="mb-8 mt-8 w-full">
+          <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
+            Achievements
+          </h4>
+          <ul className="ml-12 mt-2 list-disc  px-2 text-xl text-gray-800 ">
+            <li>Ring magazine Prospect of the Year (1985)</li>
+            <li>2× Ring magazine Fighter of the Year (1986, 1988)</li>
+            <li>2× Sugar Ray Robinson Award winner (1987, 1989)</li>
+            <li>
+              BBC Sports Personality of the Year Overseas Personality (1989)
+            </li>
+            <li>International Boxing Hall of Fame inductee (Class of 2011)</li>
+          </ul>
+        </div> */}
+  
+        <div className="mb-8 mt-8 w-full">
+          <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
+            A little about me
+          </h4>
+          <p className="ml-5 mt-2 px-2 text-xl text-gray-800">
+            {/* Michael Gerard Tyson is an American former professional boxer who
+            competed from 1985 to 2005..... */}
+                {this.state.customer.more_description}
           </p>
         </div>
-
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col items-start justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Education</p>
-          <p className="text-lg font-medium text-white dark:text-white">
-            Stanford University
-          </p>
-        </div>
-
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Languages</p>
-          <p className="text-lg font-medium text-white dark:text-white">
-            English - Spanish - Italian
-          </p>
-        </div>
-
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col items-start justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Location</p>
-          <p className="text-lg font-medium text-white dark:text-white">
-            USA - LA
-          </p>
-        </div>
-
-        <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-700 flex flex-col justify-center rounded-2xl bg-gray-800 bg-clip-border px-3 py-4 dark:shadow-none">
-          <p className="text-base text-gray-600">Phone number</p>
-          <p className="text-lg font-medium text-white dark:text-white">
-            09123456789
-          </p>
-        </div>
-      </div>
-      {/* 
-      <div className="mb-8 mt-8 w-full">
-        <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
-          Achievements
-        </h4>
-        <ul className="ml-12 mt-2 list-disc  px-2 text-xl text-gray-800 ">
-          <li>Ring magazine Prospect of the Year (1985)</li>
-          <li>2× Ring magazine Fighter of the Year (1986, 1988)</li>
-          <li>2× Sugar Ray Robinson Award winner (1987, 1989)</li>
-          <li>
-            BBC Sports Personality of the Year Overseas Personality (1989)
-          </li>
-          <li>International Boxing Hall of Fame inductee (Class of 2011)</li>
-        </ul>
-      </div> */}
-
-      <div className="mb-8 mt-8 w-full">
-        <h4 className="px-2 text-2xl font-bold text-gray-800 dark:text-white">
-          A little about me
-        </h4>
-        <p className="ml-5 mt-2 px-2 text-xl text-gray-800">
-          Michael Gerard Tyson is an American former professional boxer who
-          competed from 1985 to 2005.....
-        </p>
-      </div>
-    </Card>
-  );
-};
-
+      </Card>
+    );
+  }
+}
+ 
 export default General;
