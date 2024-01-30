9bc2911c2ecd633ad570b77c86d1eced1709cf8f
Arrash03 <atv1382@gmail.com>
2024-01-30 10:59:36 +0330

    mainpage connected to gym panel

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
                 <Route path="/coach*" element={<CoachPanel/>}/>
                 <Route path="/gympanel/:id" element={<Gym/>}/>
                 <Route path="/about" element={<AboutUs/>}/>
-                <Route path="/gympanel" element={<Gym/>}/>
+                <Route path="/gympanel/:id" element={<Gym/>}/>
                 <Route path="/about" element={<AboutUs/>}/>
                 <Route path="/search" element={<Search/>}/>
                 <Route path="/*" element={<NotFound/>}/>
